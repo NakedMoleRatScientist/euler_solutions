9c3653279a5e7d15719d92b4f01f36e494c91c22
Kiba <hackerkiba@gmail.com>
2016-08-17 17:55:22 -0400

    generalize into a method

--- solution_3.rb
+++ solution_3.rb
@@ -1,7 +1,10 @@
 num = 13195
 
-(2..(num - 1)).each do |n|
-  if num % n == 0
-    puts n
+def trial_divison number
+  list = []
+  (2..(number - 1)).each do |n|
+    if number % n == 0
+      list.push(n)
+    end
   end
 end
