fbc8340786212b416a70875d00a5d15637629800
Kiba <hackerkiba@gmail.com>
2016-08-17 18:08:37 -0400

    try to reduce # of operations needed

--- solution_3.rb
+++ solution_3.rb
@@ -1,22 +1,22 @@
 num = 13195
 
+def biggest_prime number
+  ((number - 1)..2).each do |n|
+    if number % n == 0 and trial_divison(number).size == 0
+      return n
+    end
+  end
+end
+
 def trial_division number
   list = []
   (2..(number - 1)).each do |n|
     if number % n == 0
+      puts n
       list.push(n)
     end
   end
   list
 end
 
-factors = trial_division(num)
-
-prime_factors = []
-factors.each do |f|
-  if trial_division(f).size == 0
-    prime_factors.push(f)
-  end
-end
-
-puts prime_factors
+puts biggest_prime(num)
