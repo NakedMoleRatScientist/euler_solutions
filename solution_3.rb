9a0f16109fa828c2a0cd3289d33e9105e59d6b6c
Kiba <hackerkiba@gmail.com>
2016-08-17 17:59:46 -0400

    get a list of all prime factors

--- solution_3.rb
+++ solution_3.rb
@@ -1,6 +1,6 @@
 num = 13195
 
-def trial_divison number
+def trial_division number
   list = []
   (2..(number - 1)).each do |n|
     if number % n == 0
@@ -9,3 +9,14 @@ def trial_divison number
   end
   list
 end
+
+factors = trial_division(num)
+
+prime_factors = []
+factors.each do |f|
+  if trial_division(f).size == 0
+    prime_factors.push(f)
+  end
+end
+
+puts prime_factors
